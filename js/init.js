5c328ac5538a5e63210b28bd3f1c4a74df83ce7c
chat43lepr <luisperez220322@gmail.com>
2023-11-16 10:55:38 -0600

    cambios

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyAJkfq1l_RcD8xau2pXyiG-NHIt85KjbtI",
+    authDomain: "prueba43lepr.firebaseapp.com",
+    projectId: "prueba43lepr",
+    storageBucket: "prueba43lepr.appspot.com",
+    messagingSenderId: "490435070373",
+    appId: "1:490435070373:web:4e027cec3c017e4098c04c",
+    measurementId: "G-MX4XQ208Z8"
 });
